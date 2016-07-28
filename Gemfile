e2880b4b6a8c3b7595899cabec65950271236de4
Trey Evans <lewis.r.evans@gmail.com>
2016-07-28 16:40:18 -0400

    Adding map file.

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem "react-rails"
 # gem 'capistrano-rails', group: :development
 
 gem 'bootstrap', '~> 4.0.0.alpha3'
+gem 'font-awesome-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/components/route_argument_form.js.jsx
+++ app/assets/javascripts/components/route_argument_form.js.jsx
@@ -14,20 +14,18 @@ var RouteArgumentForm = React.createClass({
        id_field = <input type="hidden" name={prefixName + "[id]"} value={id} />
      }
      return(
-        <div>
+        <fieldset>
+	  <legend>Argument <i className="fa fa-times" onClick={parentForm.deleteArg} data-index={index}></i></legend>
 	  {id_field}
-	  <div className="field">
+	  <div className="form-group">
 	    <label htmlFor={prefixId + "_key"}>Key</label>
-                <input type="text" name={prefixName + "[key]"} id={prefixId + "_key"} defaultValue={key}/>
+            <input className="form-control" type="text" name={prefixName + "[key]"} id={prefixId + "_key"} defaultValue={key}/>
 	  </div>
-	  <div className="field">
+	  <div className="form-group">
 	    <label htmlFor={prefixId + "_value"}>Value</label>
-                <input type="text" name={prefixName + "[value]"} id={prefixId + "_value"} defaultValue={value}/>
+            <input className="form-control" type="text" name={prefixName + "[value]"} id={prefixId + "_value"} defaultValue={value}/>
 	  </div>
-	  <p>
-            <a href="#" onClick={parentForm.deleteArg} data-index={index}>Delete</a>
-	  </p>
-	</div>
+	</fieldset>
      );
   }
 });
