368aceda2c9e3d799e9b34199857a92ef9daec31
Trey Evans <lewis.r.evans@gmail.com>
2016-08-02 10:40:57 -0400

    Adding junit test results.

--- react_spec/run_tests.js
+++ react_spec/run_tests.js
@@ -6,6 +6,14 @@ global.react_helper_path = __dirname + '/support/react_helper';
 
 var jrunner = new Jasmine();
 jrunner.configureDefaultReporter({print: noop});
+
+var reporters = require('jasmine-reporters');
+var junitReporter = new reporters.JUnitXmlReporter({
+        savePath: __dirname + "/test_results",
+        consolidateAll: false
+});
+
+jrunner.addReporter(junitReporter);
 jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'all'}));
 jrunner.loadConfigFile('support/compiled/spec/support/jasmine.json');
 jrunner.execute();
